07041164c2bff043189b343b3ad08f702f86e43c
HebaRouk <113283778+HebaRouk@users.noreply.github.com>
2024-08-02 06:58:17 +0400

    Update adjust_results4_isadog.py

--- GRE/data/adjust_results4_isadog.py
+++ GRE/data/adjust_results4_isadog.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/adjust_results4_isadog.py
 #                                                                             
-# PROGRAMMER: Fatima AbuReesh
-# DATE CREATED:   25/52024                              
+# PROGRAMMER: Heba Rouk
+# DATE CREATED:  2/8/2024                              
 # REVISED DATE: 
 # PURPOSE: Create a function adjust_results4_isadog that adjusts the results 
 #          dictionary to indicate whether or not the pet image label is of-a-dog, 
