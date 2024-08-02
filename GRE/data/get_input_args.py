b59aa7b4e42c9986a7989e7d4cf35aeac0ee33c4
HebaRouk <113283778+HebaRouk@users.noreply.github.com>
2024-08-02 07:15:23 +0400

    Update get_input_args.py

--- GRE/data/get_input_args.py
+++ GRE/data/get_input_args.py
@@ -50,3 +50,4 @@ def get_input_args():
     # Replace None with parser.parse_args() parsed argument collection that 
     # you created with this function 
     return parser.parse_args()
+#################################################
