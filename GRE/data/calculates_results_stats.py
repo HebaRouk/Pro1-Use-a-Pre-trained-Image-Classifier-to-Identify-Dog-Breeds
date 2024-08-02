749eba948abe834b35e9fb64c751ef4218b5ebae
HebaRouk <113283778+HebaRouk@users.noreply.github.com>
2024-08-02 07:00:16 +0400

    Update calculates_results_stats.py

--- GRE/data/calculates_results_stats.py
+++ GRE/data/calculates_results_stats.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/calculates_results_stats.py
 #                                                                             
-# PROGRAMMER:Fatima AbuReesh
-# DATE CREATED:  26/5/2024                                
+# PROGRAMMER:Heba Rouk
+# DATE CREATED:2/8/2024                                
 # REVISED DATE: 
 # PURPOSE: Create a function calculates_results_stats that calculates the 
 #          statistics of the results of the programrun using the classifier's model 
