980a4a32c714f837124fdc680119c18d6e15995b
HebaRouk <113283778+HebaRouk@users.noreply.github.com>
2024-08-02 07:18:26 +0400

    Update run_models_batch.sh

--- GRE/data/run_models_batch.sh
+++ GRE/data/run_models_batch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # */AIPND-revision/intropyproject-classify-pet-images/run_models_batch.sh
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                 
+# PROGRAMMER: Heba Rouk
+# DATE CREATED: 2/8/2024                                
 # REVISED DATE:  
 # PURPOSE: Runs all three models to test which provides 'best' solution.
 #          Please note output from each run has been piped into a text file.
