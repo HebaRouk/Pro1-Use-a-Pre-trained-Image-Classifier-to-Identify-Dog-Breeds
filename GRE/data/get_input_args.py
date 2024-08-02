7105b9b00b1365d2b45a3dbeeb57a0fa91414dc1
HebaRouk <113283778+HebaRouk@users.noreply.github.com>
2024-08-02 07:06:01 +0400

    Update get_input_args.py

--- GRE/data/get_input_args.py
+++ GRE/data/get_input_args.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: Fatima AbuReesh
-# DATE CREATED:  22/5/2024                                 
+# PROGRAMMER: Heba Rouk
+# DATE CREATED:  2/8/2024                                
 # REVISED DATE: 
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
